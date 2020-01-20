113612d0bd03ba380ed251fe7208f1dd44f8a581
smiyaguchi <shinichi.miyaguchi@topgate.co.jp>
2020-01-20 15:35:47 +0900

    Add faker

---
+++ generator/faker/faker.go
@@ -0,0 +1,66 @@
+package faker
+
+import (
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/brianvoe/gofakeit"
+	"github.com/smiyaguchi/headwater/schema"
+)
+
+var keys = make(map[string]int)
+
+type Data struct {
+	ColumnValue map[string]string
+	RowValue    []string
+	Key         string
+}
+
+func Fake(schema schema.Schema) Data {
+	cv := make(map[string]string)
+	rv := make([]string, len(schema.Columns))
+	key := ""
+	gofakeit.Seed(time.Now().UnixNano())
+
+	for i, c := range schema.Columns {
+		if !c.Unique && c.Mode == "NULLABLE" {
+			rand.Seed(time.Now().UnixNano())
+			if rand.Intn(10) > 8 {
+				continue
+			}
+		}
+		
+		d := ""
+		t := strings.ToUpper(c.Type)
+		if t == "STRING" {
+			d = gofakeit.Name() + "_" + gofakeit.City()
+		} else if t == "INTEGER" {
+			d = strconv.FormatUint(gofakeit.Uint64(), 10)
+		} else if t == "NUMERIC" {
+			d = strconv.FormatInt(gofakeit.Int64(), 10)
+		} else if t == "FLOAT" {
+			d = strconv.FormatFloat(gofakeit.Float64(), 'e', 9, 64)
+		} else if t == "BOOLEAN" {
+			d = strconv.FormatBool(gofakeit.Bool())
+		} else if t == "TIMESTAMP" {
+			d = gofakeit.Date().String()
+		} else if t == "DATE" {
+			d = gofakeit.Date().Format("2006-01-02")
+		} else if t == "TIME" {
+			d = gofakeit.Date().Format("15:04:05")
+		} else if t == "DATETIME" {
+			d = gofakeit.Date().Format("2006-01-02 15:04:05")
+		}
+
+		cv[c.Name] = d
+		rv[i] += d
+
+		if c.Unique {
+			key += d
+		}
+	}
+
+	return Data{ColumnValue: cv, RowValue: rv, Key: key}
+}
--- generator/normal_generator.go
+++ generator/normal_generator.go
@@ -2,71 +2,34 @@ package generator
 
 import (
 	"encoding/csv"
-	"math/rand"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
-	"github.com/smiyaguchi/headwater/schema"
 	"github.com/brianvoe/gofakeit"
+	"github.com/smiyaguchi/headwater/generator/faker"
+	"github.com/smiyaguchi/headwater/schema"
 )
 
 var keys = make(map[string]int)
 
 func Generate(schema schema.Schema, count int) {
-	data := make([][]string, count)	
+	data := make([][]string, count)
 	gofakeit.Seed(time.Now().UnixNano())
 
 	for i := 0; i < count; i++ {
-		d := make([]string, len(schema.Columns))
-		key := ""
-		for j, v := range schema.Columns {
-			if !v.Unique && v.Mode == "NULLABLE" {
-				rand.Seed(time.Now().UnixNano())
-				if rand.Intn(10) > 8 {
-					continue
-				}				
-			}
+		d := faker.Fake(schema)
 
-			t := strings.ToUpper(v.Type)
-			if t == "STRING" {
-				d[j] = gofakeit.Name() + "_" + gofakeit.City()
-			} else if t == "INTEGER" {
-				d[j] = strconv.FormatUint(gofakeit.Uint64(), 10)
-			} else if t == "NUMERIC" {
-				d[j] = strconv.FormatInt(gofakeit.Int64(), 10)
-			} else if t == "FLOAT" {
-				d[j] = strconv.FormatFloat(gofakeit.Float64(), 'e', 9, 64)
-			} else if t == "NUMERIC" {
-				d[j] = strconv.FormatFloat(gofakeit.Float64(), 'e', 9, 64)
-			} else if t == "BOOLEAN" {
-				d[j] = strconv.FormatBool(gofakeit.Bool())
-			} else if t == "TIMESTAMP" {
-				d[j] = gofakeit.Date().String()	
-			} else if t == "DATE" {
-				d[j] = gofakeit.Date().Format("2006-01-02")
-			} else if t == "TIME" {
-				d[j] = gofakeit.Date().Format("15:04:05")
-			} else if t == "DATETIME" {
-				d[j] = gofakeit.Date().Format("2006-01-02 15:04:05")
-			}
-			
-			if v.Unique {
-				key += d[j]
-			}
-		}
-		if _, exist := keys[key]; exist {
+		if _, exist := keys[d.Key]; exist {
 			i--
 			continue
 		}
-		data[i] = d
-		keys[key] = 0
+		data[i] = d.RowValue
+		keys[d.Key] = 0
 	}
-	
+
 	w := csv.NewWriter(os.Stdout)
 	w.WriteAll(data)
-	
+
 	if err := w.Error(); err != nil {
 		panic(err)
 	}
